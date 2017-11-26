9705929939fc8ca7519f3b2b28d71ed37ad15061
Swarnim Das <1405266@kiit.ac.in>
2017-11-26 23:28:31 +0530

    Created Login Page SignIn Authentication done Tabs Page Dashboard completed

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+  apiKey: "AIzaSyD1GVenJ5ePQMKACiY4wsyNnSQKeVXALo0",
+  authDomain: "ionic3chat-af9fb.firebaseapp.com",
+  databaseURL: "https://ionic3chat-af9fb.firebaseio.com",
+  projectId: "ionic3chat-af9fb",
+  storageBucket: "ionic3chat-af9fb.appspot.com",
+  messagingSenderId: "346103601131"
+};
